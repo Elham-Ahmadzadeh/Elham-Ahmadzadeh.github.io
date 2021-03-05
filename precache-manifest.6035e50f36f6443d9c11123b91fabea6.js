1c361dc858891d7ce349e0ae9739e8957d8de18f
Elham Ahmadzadeh <eliahm63@gmail.com>
2021-03-05 19:34:44 +0100

    init

---
+++ precache-manifest.6035e50f36f6443d9c11123b91fabea6.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caca5fec3fee8f629739",
+    "url": "/css/app.1abe3138.css"
+  },
+  {
+    "revision": "56ecfe1f4fb83373bef25e6fdf71ce6d",
+    "url": "/data.json"
+  },
+  {
+    "revision": "964111990ca3937960147b80e8d07e61",
+    "url": "/fonts/ARCADECLASSIC.96411199.TTF"
+  },
+  {
+    "revision": "4f44aee0aa4ee6a6617c1a7af858a0b5",
+    "url": "/img/Background1.4f44aee0.svg"
+  },
+  {
+    "revision": "cd091f35a544ae8415995ee028510fa1",
+    "url": "/img/cardBack.cd091f35.jpg"
+  },
+  {
+    "revision": "53c9962a87d14f4d6e37ed4878013f1e",
+    "url": "/img/logo-dark.53c9962a.svg"
+  },
+  {
+    "revision": "51782014870159418ceb8a7087a08566",
+    "url": "/img/mastermind.51782014.png"
+  },
+  {
+    "revision": "249458b94ebbde43e61a75993cda9a1b",
+    "url": "/img/mastermindd.249458b9.png"
+  },
+  {
+    "revision": "16bbb2c772b1b50eee21101326778a99",
+    "url": "/img/menu-btn.16bbb2c7.svg"
+  },
+  {
+    "revision": "78e3e3148344f4128a5419a011d3a50a",
+    "url": "/img/snakegame.78e3e314.png"
+  },
+  {
+    "revision": "ba195d0307cb4dedad0b50de23088cd0",
+    "url": "/img/space-invadors.ba195d03.png"
+  },
+  {
+    "revision": "b328927abc394f0cad4e1eee7f942430",
+    "url": "/img/tictactoe.b328927a.png"
+  },
+  {
+    "revision": "4c93162369de99fbf53811138a749797",
+    "url": "/index.html"
+  },
+  {
+    "revision": "caca5fec3fee8f629739",
+    "url": "/js/app.565b1f5c.js"
+  },
+  {
+    "revision": "129e978bfc871e06f425",
+    "url": "/js/chunk-vendors.1e2b8395.js"
+  },
+  {
+    "revision": "5c4a109e2dffb7b615f86698fe028cf8",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "84eb264a6fc808a2e38b99d42b80eeff",
+    "url": "/media/eat.84eb264a.wav"
+  },
+  {
+    "revision": "95ec9f2e14fb384c679699d55938d5bb",
+    "url": "/media/enter.95ec9f2e.wav"
+  },
+  {
+    "revision": "b626911f1a383dc9399f7d7245ff5a80",
+    "url": "/media/shoot.b626911f.wav"
+  },
+  {
+    "revision": "7ef960461ab8dad183e4731a914b3d29",
+    "url": "/media/start.7ef96046.wav"
+  },
+  {
+    "revision": "e4fb02c79e61849a9a2cd207e8589bbc",
+    "url": "/media/win.e4fb02c7.wav"
+  },
+  {
+    "revision": "8fb75c98bb5ee8764a33b368b68caeb5",
+    "url": "/media/win2.8fb75c98.wav"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "421273e7b7b1deca203598e3d1d1f9dc",
+    "url": "/svg/ace-of-clubs.svg"
+  },
+  {
+    "revision": "abd091a50343d170e759f800ffcd357e",
+    "url": "/svg/ace-of-diamonds.svg"
+  },
+  {
+    "revision": "8771c36b0fc947c718f044d6ea4f85cf",
+    "url": "/svg/ace-of-hearts.svg"
+  },
+  {
+    "revision": "ec65f40b3de592f742fc331c81ea4f7d",
+    "url": "/svg/ace-of-spades.svg"
+  },
+  {
+    "revision": "a4d3982360d7edbcdb56afdbc3650eda",
+    "url": "/svg/back.svg"
+  },
+  {
+    "revision": "ab5e43e1630de0614b6e29c227d49890",
+    "url": "/svg/backside.jpg"
+  },
+  {
+    "revision": "305439fd83a5eb9fe9b36ba92a1ce614",
+    "url": "/svg/king-of-clubs.svg"
+  },
+  {
+    "revision": "af23bb5a8a1320f9c39e9e9be5422915",
+    "url": "/svg/king-of-diamonds.svg"
+  },
+  {
+    "revision": "8f5d9a6c86056c29626d919c3d20cef8",
+    "url": "/svg/king-of-hearts.svg"
+  },
+  {
+    "revision": "037122395b514d9ca33d373ff4bf6c2e",
+    "url": "/svg/king-of-spades.svg"
+  },
+  {
+    "revision": "c513b4aaca25af82f008854f8a279844",
+    "url": "/svg/queen-of-clubs.svg"
+  },
+  {
+    "revision": "e25fc884ba8e021c42e001b255ca12f8",
+    "url": "/svg/queen-of-diamonds.svg"
+  },
+  {
+    "revision": "49d3c1e8f1745d4fecb45d9394bc7689",
+    "url": "/svg/queen-of-hearts.svg"
+  },
+  {
+    "revision": "83ca07e2292b2ace10940a7e41bac0e1",
+    "url": "/svg/queen-of-spades.svg"
+  }
+]);
